62e90e4c5ad6ddefdade8c721430f28cba763292
GeneBean <me@technicalissues.us>
2014-12-23 18:06:44 -0500

    added a message to the pending line

--- spec/moddeps_spec.rb
+++ spec/moddeps_spec.rb
@@ -5,7 +5,7 @@ describe Puppet::Moddeps do
   describe '.installModuleDependencies(puppet_module)' do
 
     it 'installs the dependencies of a module' do
-      pending
+      pending("This test isn't setup yet")
     end
 
   end
