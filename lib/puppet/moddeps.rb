19f6cf6cc6d393d9a429933feb2a2009ced363f9
GeneBean <me@technicalissues.us>
2014-12-27 23:04:25 +0000

    The first two tests are passing now.

--- lib/puppet/moddeps.rb
+++ lib/puppet/moddeps.rb
@@ -6,8 +6,19 @@ module Puppet
   module Moddeps
 
     @@default_module_path = '/etc/puppet/modules'
+    
+    def Moddeps.installDeps(*puppet_module)
+      if ( puppet_module.nil? or puppet_module.size == 0 or puppet_module.size > 1)
+        Moddeps.help
+      end
+    end
+    
+    def Moddeps.help
+      puts 'Usage: puppet-moddeps module'
+      puts '       Call puppet-moddeps with the name of one installed module'
+    end
 
-    def self.installModuleDependencies(puppet_module)
+    def Moddeps.installModuleDependencies(puppet_module)
 
       @puppet_module = puppet_module
       @metadata      = File.read("#{@@default_module_path}/#{@puppet_module}/metadata.json")
--- spec/moddeps_spec.rb
+++ spec/moddeps_spec.rb
@@ -1,45 +1,50 @@
 require 'spec_helper'
 
 describe Puppet::Moddeps do
+  
+  describe '.installDeps(puppet_module)' do
 
-  context 'no parameters' do
-    it 'should print usage info' do
-      pending('test that usage info was printed')
-    end
-  end
-  
-  context 'two or more parameters' do
-    it 'should print usage info' do
-      pending('test that usage info was printed')
-    end
-  end
-  
-  context 'one parameter' do
-    it 'should verify parameter is the name of an installed module' do
-      if false
-        it "should print usage and exit 1" do
-          pending('test for usage being printed')
-          pending('verify exit code is 1')
-        end
+    context 'with no parameters' do
+      it 'should print usage info' do
+        
+        expect { Puppet::Moddeps.installDeps() }.to output(/Usage.*/).to_stdout
       end
     end
     
-    it "should parse metadata.json" do
-      pending('create array of deps')
+    context 'with two or more parameters' do
+      it 'should print usage info' do
+        expect { Puppet::Moddeps.installDeps('arg1', 'arg2') }.to output(/Usage.*/).to_stdout
+      end
     end
     
-    it "should return an array of modules to install" do
-      pending('test that an array is returned, even if zero elements')
-    end
-    
-    it "should install each module in provided array" do
-      if File.directory?('some module')
-        it "it should notify that module exists" do
-          pending('test that notification was printed')
+    context 'with one parameter' do
+      it 'should verify parameter is the name of an installed module' do
+        if false
+          it "should print usage and exit 1" do
+            pending('test for usage being printed')
+            pending('verify exit code is 1')
+          end
+        end
+      end
+      
+      it "should parse metadata.json" do
+        pending('create array of deps')
+      end
+      
+      it "should return an array of modules to install" do
+        pending('test that an array is returned, even if zero elements')
+      end
+      
+      it "should install each module in provided array" do
+        if File.directory?('some module')
+          it "it should notify that module exists" do
+            pending('test that notification was printed')
+          end
+          
+        else
+          pending('test that module was installed')
         end
         
-      else
-        pending('test that module was installed')
       end
       
     end
