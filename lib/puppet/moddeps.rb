1246ad1f221340976b6600f18e4989b1218749d2
GeneBean <me@technicalissues.us>
2014-12-28 06:13:45 +0000

    removed unused old code

--- lib/puppet/moddeps.rb
+++ lib/puppet/moddeps.rb
@@ -61,21 +61,6 @@ module Puppet
       end
     end
 
-    def Moddeps.installModuleDependencies(puppet_module)
-
-      @puppet_module = puppet_module
-      @metadata      = File.read("#{@@default_module_path}/#{@puppet_module}/metadata.json")
-      @data          = JSON.parse(@metadata)
-
-      @data['dependencies'].each do |dep|
-        @note    = 'Installing dependency'
-        @depname = dep["name"].sub '/', '-'
-        @cmd     = "puppet module install #{@depname}"
-        puts "#{@cmd}"
-        exec("#{@cmd}")
-      end
-    end
-
   end
 end
 
