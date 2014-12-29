d50c680c553358bfc95f415413b25badaee2837b
GeneBean <me@technicalissues.us>
2014-12-29 13:24:28 -0500

    Prefixed method calls with self, made get_deps a private method.

--- lib/puppet/moddeps/install_deps.rb
+++ lib/puppet/moddeps/install_deps.rb
@@ -20,13 +20,13 @@ module Puppet
 
         if args.size == 1
           if check_if_installed(args[0])
-            parse_metadata(args[0])
-            install_modules
+            self.parse_metadata(args[0])
+            self.install_modules
           else
             puts "Can't find #{args[0]} in #{@default_module_path}"
           end
         else
-          help
+          self.help
         end
       end
 
@@ -42,23 +42,13 @@ module Puppet
       def parse_metadata(puppet_module)
         metadata = File.read("#{@default_module_path}/#{puppet_module}/metadata.json")
         data     = JSON.parse(metadata)
-        @deps    = Moddeps.get_deps(data)
-      end
-
-      def get_deps(data)
-        dependencies = []
-        data['dependencies'].each do |dep|
-          depname = dep["name"].sub '/', '-'
-          dependencies.push( depname )
-        end
-
-        return dependencies
+        @deps    = self.get_deps(data)
       end
 
       def install_modules
         if @deps.size > 0
           @deps.each do |dep|
-            if Moddeps.check_if_installed(dep)
+            if self.check_if_installed(dep)
               puts "#{dep} is already installed, skipping."
             else
               cmd = "/usr/bin/puppet module install #{dep}"
@@ -69,6 +59,18 @@ module Puppet
         else
           puts 'No dependencies were marked for installation.'
         end
+      end
+
+      private
+
+      def get_deps(data)
+        dependencies = []
+        data['dependencies'].each do |dep|
+          depname = dep["name"].sub '/', '-'
+          dependencies.push( depname )
+        end
+
+        return dependencies
       end
 
     end
