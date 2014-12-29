24a1cbd71c725ba6a1169c2871c657910a67c127
GeneBean <me@technicalissues.us>
2014-12-28 20:35:52 -0500

    Rework travis setup

--- scripts/ubuntu12.04-puppet.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-wget https://apt.puppetlabs.com/puppetlabs-release-precise.deb
-dpkg -i puppetlabs-release-precise.deb
-rm -f puppetlabs-release-precise.deb
-apt-get update -qq
-apt-get install -y puppet > /dev/null
