590e65926e8daaaaa08cc063cb8ecc20900c9519
GeneBean <me@technicalissues.us>
2014-12-29 22:05:10 -0500

    Updating build env

--- scripts/pretest.sh
+++ scripts/pretest.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-chmod 777 /etc/puppet/modules
+moddir="`puppet config print modulepath | cut -d ':' -f1`"
+modname='fake_private_module'
 
-moddir='/etc/puppet/modules/fake_private_module'
+if [ -d "$moddir/$modname" ]; then rm -rf "$moddir"; fi
 
-if [ -d "$moddir" ]; then rm -rf "$moddir"; fi
+mkdir -p "$moddir/$modname"
 
-mkdir "$moddir"
+chmod -R 775 "$moddir/$modname"
 
-cat > "$moddir/metadata.json" <<EOF
+cat > "$moddir/$modname/metadata.json" <<EOF
 {
   "dependencies": [
     {"name":"puppetlabs/stdlib","version_requirement":">= 2.4.0"},
     {"name":"puppetlabs/concat","version_requirement":">= 1.1.1"},
     {"name":"puppetlabs/firewall","version_requirement":">= 1.3.0"}
-  ]
+  ],
 }
 
 EOF
