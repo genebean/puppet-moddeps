dcc34c9565b7c815f7e4d444f2d59d9191fe9bc1
GeneBean <me@technicalissues.us>
2014-12-30 00:09:15 -0500

    Updating based on support email from Code Climate. The order seems to effect their test coverage framework. With the old order no data was being posted from Travis-CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
-
 require 'coveralls'
 Coveralls.wear!
+
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
 
 require 'pry'
 require 'puppet/moddeps'
