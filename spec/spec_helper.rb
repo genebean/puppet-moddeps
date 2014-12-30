089ab8302a2f621f24a7812d2016d8facf2b4325
GeneBean <me@technicalissues.us>
2014-12-30 14:53:02 -0500

    Commented code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,12 +2,13 @@ require 'codeclimate-test-reporter'
 require 'coveralls'
 require 'simplecov'
 
+# The order of these is very important for making them all work together.
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
   Coveralls::SimpleCov::Formatter,
   SimpleCov::Formatter::HTMLFormatter,
-  CodeClimate::TestReporter::Formatter
+  CodeClimate::TestReporter::Formatter # this has to be last or reporting fails.
 ]
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' # without 'rails' this doesn't work right.
 
 
 require 'pry'
